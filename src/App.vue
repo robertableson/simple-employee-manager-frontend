3b67c3fc13d4b5993b109805e81ce27f76e65aa5
Robert Ableson <robertableson@gmail.com>
2017-11-15 16:43:10 -0500

    Basic vuetify setup

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
-  <v-app light>
-    <v-navigation-drawer
+  <v-app>
+
+    <v-navigation-drawer dark
       fixed
       :mini-variant="miniVariant"
       v-model="drawer"
       app
     >
       <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :key="i"
-          value="true"
-        >
+        <v-list-tile avatar v-for="item in items" v-bind:key="item.title" @click="">
           <v-list-tile-action>
-            <v-icon light v-html="item.icon"></v-icon>
+            <v-icon v-if="item.icon" v-html="item.icon"></v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title v-text="item.title"></v-list-tile-title>
@@ -21,35 +18,17 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
+
     <v-toolbar app>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+
+      <v-btn icon @click.stop="drawer = !drawer">
+        <v-icon v-html="drawer ? 'chevron_left' : 'menu'"></v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
+
       <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
+
     </v-toolbar>
+
     <v-content>
       <v-container fluid>
         <v-slide-y-transition mode="out-in">
@@ -67,22 +46,7 @@
         </v-slide-y-transition>
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      fixed
-      :right="right"
-      v-model="rightDrawer"
-      app
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
+
     <v-footer :fixed="fixed" app>
       <span>&copy; 2017</span>
     </v-footer>
@@ -94,16 +58,15 @@
     data () {
       return {
         clipped: false,
-        drawer: true,
+        drawer: false,
         fixed: false,
-        items: [{
-          icon: 'bubble_chart',
-          title: 'Inspire'
-        }],
+        items: [
+          {icon: 'dashboard', title: 'Tableau de bord'},
+          {icon: 'people', title: 'Employés'}
+        ],
         miniVariant: false,
         right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+        title: 'Vous inc.'
       }
     }
   }
