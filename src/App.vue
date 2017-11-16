e8768f184b478a6a1fd2cefd8d4bfa74d0008a1a
Robert Ableson <robertableson@gmail.com>
2017-11-15 22:16:15 -0500

    Added main component

--- src/App.vue
+++ src/App.vue
@@ -29,23 +29,15 @@
 
     </v-toolbar>
 
-    <v-content>
-      <v-container fluid>
-        <v-slide-y-transition mode="out-in">
-          <v-layout column align-center>
-            <img src="/static/v.png" alt="Vuetify.js" class="mb-5" />
-            <blockquote>
-              &#8220;First, solve the problem. Then, write the code.&#8221;
-              <footer>
-                <small>
-                  <em>&mdash;John Johnson</em>
-                </small>
-              </footer>
-            </blockquote>
-          </v-layout>
-        </v-slide-y-transition>
-      </v-container>
-    </v-content>
+    <main>
+      <v-content>
+        <v-container fluid>
+          <v-slide-y-transition mode="out-in">
+            <router-view></router-view>
+          </v-slide-y-transition>
+        </v-container>
+      </v-content>
+    </main>
 
     <v-footer app>
       <span>&copy; 2017</span>
@@ -67,7 +59,7 @@
           miniVariant: false
         },
         header: {
-          title: 'Vous inc.'
+          title: 'Gestion 2000'
         }
       }
     }
