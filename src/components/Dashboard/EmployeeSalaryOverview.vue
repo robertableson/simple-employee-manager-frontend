cf8396e2e03aac000fa0de6c5ca6c2a14a50fbd7
Robert Ableson <robertableson@gmail.com>
2017-11-20 18:23:49 -0500

    Added test cards for dashboard

---
+++ src/components/Dashboard/EmployeeSalaryOverview.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container
+    fluid
+    style="min-height: 0;"
+    grid-list-lg
+  >
+    <v-layout row wrap>
+      <v-flex m6>
+        <v-card color="green" class="white--text elevation-3">
+          <v-card-text>
+            <p class="display-1 text-xs-right mb-0 pt-3">76 $</p>
+            <p class="text-xs-right">Coût horaire</p>
+            <p class="display-1 text-xs-right mb-0 pt-3">7687 $</p>
+            <p class="text-xs-right">Coût mensuel</p>
+            <p class="display-1 text-xs-right mb-0 pt-3">768765 $</p>
+            <p class="text-xs-right">Coût annuel</p>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+
+      <v-flex m6>
+        <v-card raised color="blue-grey darken-2" class="white--text elevation-3">
+          <v-card-text>
+            <p class="display-1 text-xs-right mb-0 pt-3">5</p>
+            <p class="text-xs-right">Employés</p>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+
+      <v-flex m6>
+        <v-card raised color="blue-grey darken-2" class="white--text elevation-3">
+          <v-card-text>
+            <p class="display-1 text-xs-right mb-0 pt-3">5</p>
+            <p class="text-xs-right">Employés</p>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+
+      <v-flex m6>
+        <v-card raised color="blue-grey darken-2" class="white--text elevation-3">
+          <v-card-text>
+            <p class="display-1 text-xs-right mb-0 pt-3">5</p>
+            <p class="text-xs-right">Employés</p>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+
+      <v-flex m6>
+        <v-card raised color="blue-grey darken-2" class="white--text elevation-3">
+          <v-card-text>
+            <p class="display-1 text-xs-right mb-0 pt-3">5</p>
+            <p class="text-xs-right">Employés</p>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
--- src/components/Dashboard/MainDashboard.vue
+++ src/components/Dashboard/MainDashboard.vue
@@ -1,9 +1,16 @@
 <template>
-
+  <div>
+    <employee-salary-overview></employee-salary-overview>
+  </div>
 </template>
 
 <script>
+import EmployeeSalaryOverview from './EmployeeSalaryOverview.vue'
+
 export default {
+  components: {
+    'employee-salary-overview': EmployeeSalaryOverview
+  },
   data () {
     return {
     }
